9951fff1bcdcce006970b2b26548a546d8b85d0a
hussein <fathinsatrian358@gmail.com>
2022-04-13 11:52:14 +0700

    folder structurized

---
+++ employee_system/src/App/App.js
@@ -0,0 +1,14 @@
+import LoginForm from '../components/LoginForm';
+import SideMenu from '../components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu/>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ employee_system/src/components/SideMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SideMenu() {
+  return (
+    <div className="side-menu"/>
+  )
+}
+
+export default SideMenu
